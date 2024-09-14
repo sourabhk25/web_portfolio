52adfacaf7fa4a7b2bbbb57a07bd39d5e52f27d1
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2024-09-13 23:19:55 -0700

    Updated company link logic for InnovateMore

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -26,8 +26,23 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
     <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between md:w-[80%]">
       <LiIcon reference={ref} />
       <motion.div initial={{ y: 50 }} whileInView={{ y: 0 }} transition={{ duration: 0.5, type: "spring" }}>
-        <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+        {/* <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
           {position} <a className="capitalize text-primary dark:text-primaryDark" href={companyLink} target="_blank" rel="noopener noreferrer">@{company}</a>
+        </h3> */}
+        <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+          {position}{" "}
+          {company === "InnovateMore Inc." ? (
+            <span className="capitalize text-primary dark:text-primaryDark">@{company}</span>
+          ) : (
+            <a
+              className="capitalize text-primary dark:text-primaryDark"
+              href={companyLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @{company}
+            </a>
+          )}
         </h3>
         <span className="capitalize text-dark/75 font-medium dark:text-light/50 xs:text-sm">
           {time} | {address}
